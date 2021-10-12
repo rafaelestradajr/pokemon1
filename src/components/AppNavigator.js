17b0e72bc6c60ed1ccd153d9772f1a42b6f7384a
Rafael Estrada <rafaelestrada@comcast.net>
2021-10-11 21:23:00 -0700

    used axios for API

---
+++ src/components/AppNavigator.js
@@ -0,0 +1,39 @@
+import React from "react";
+import {AppBar, makeStyles, Toolbar, Typography} from '@material-ui/core';
+import { Link } from "react-router-dom";
+
+
+
+const useStyles = makeStyles((theme) => ({
+    AppBar:{
+        backgroundColor:'black'
+    },
+    link:{
+        textDecoration:'none'
+    },
+    title:{
+        cursor:'pointer',
+        color:'white'
+    }
+})
+)
+
+  function AppNavigator() {
+
+const classes = useStyles()
+
+  return(
+
+  <AppBar className={classes.AppBar} position='fixed' variant='h6' >
+      <Toolbar>
+
+          <Link to='/' className={classes.link}>
+              <Typography className={classes.title}>Pokedex</Typography>
+          </Link>
+      </Toolbar>
+
+  </AppBar>
+  )
+}
+
+export default AppNavigator;
---
+++ src/containers/Pokedex.js
@@ -0,0 +1,51 @@
+import React,{useEffect,useState} from 'react';
+import { Box, CircularProgress } from '@material-ui/core';
+import axios from 'axios';
+import { IMAGE_API_URL, POKEMON_APP_URL } from '../config';
+
+
+
+export default function Pokedex() {
+
+    const[pokemonData,setPokemonData] = useState(null)
+
+    useEffect(() => {
+        axios.get(POKEMON_APP_URL +'?limit=800')
+        .then((response) =>{
+            if(response.status >= 200 && response.status <300 ) {
+                const {results} = response.data
+                let newPokemonData =[]
+                results.forEach((pokemon,index) => {
+                    index++
+
+                    let pokemonObject ={
+                        id: index,
+                        url:IMAGE_API_URL + index + '.png',
+                        name: pokemon.name
+                    }
+                    newPokemonData.push(pokemonObject)
+                });
+
+                setPokemonData(newPokemonData)
+
+            }
+
+
+           
+        })
+
+
+
+    },[])
+    return (
+        
+            <Box>
+                {pokemonData ? pokemonData.map((pokemon) =>{
+                
+                return <h1>{pokemon.name}</h1>
+                 }) : <CircularProgress style={{marginTop: 100}}/>}
+
+            </Box>
+        
+    )
+}
